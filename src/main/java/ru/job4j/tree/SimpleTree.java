06247c3f112490b85918fb59ade5964f641ded44
Dinar <gizzyatovdinar@gmail.com>
2025-03-12 14:06:01 +0400

    Commit [#19]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -12,22 +13,29 @@ public class SimpleTree<E> implements Tree<E> {
     @Override
     public boolean add(E parent, E child) {
         boolean result = false;
-        Optional<Node<E>> parentNode = findBy(parent);
-        if (parentNode.isPresent() && findBy(child).isEmpty()) {
-            parentNode.get().children.add(new Node<>(child));
+        if (findBy(parent).isPresent() && findBy(child).isEmpty()) {
+            findBy(parent).get().children.add(new Node<>(child));
             result = true;
         }
         return result;
     }
 
+    public boolean isBinary() {
+        return findByPredicate(value -> value.children.size() > 2).isEmpty();
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(val -> val.value.equals(value));
+    }
+
+    public Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> result = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> element = data.poll();
-            if (element.value.equals(value)) {
+            if (condition.test(element)) {
                 result = Optional.of(element);
                 break;
             }
